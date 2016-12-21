2f61f2887d44661b3971533cacd867e84ef969c9
Kevin Colligan <kevin.colligan@macbookair-kevincolligan.local>
2016-12-21 13:52:13 -0500

    First commit

---
+++ MyPodcastApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPodcastApp
+//
+//  Created by Kevin Colligan on 12/21/16.
+//  Copyright © 2016 Kevin Colligan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
